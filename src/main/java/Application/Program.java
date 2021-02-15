1de83888d5150b99049f78eb7f0246d88dbbc0f0
Eduardo Paulo dos Reis <eduardo.reis@educacao.sp.gov.br>
2021-02-15 16:10:10 -0300

    Projeto criado

---
+++ src/main/java/Application/Program.java
@@ -0,0 +1,16 @@
+package Application;
+
+/**
+ *
+ * @author eduardo.reis
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
